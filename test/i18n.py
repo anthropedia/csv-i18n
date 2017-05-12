0c086ed92a53e3c7a236046c8f2e5497ba5ef632
Hugo MOCHET <mochet.hugo@gmail.com>
2017-05-12 16:01:58 +0200

    Add translation from csv file with hard coded file on trans module

--- test/i18n.py
+++ test/i18n.py
@@ -1,14 +1,15 @@
 from unittest import TestCase
+from tcii18n import trans
 
 
 class I18nTest(TestCase):
     def setUp(self):
-        self.trans = None
+        self.trans = trans._
 
     def test_translate(self):
-        self.assertEqual(self.trans('TCI Title'), 'Titre ITC')
+        self.assertEqual(self.trans('TCI title'), 'Titre ITC')
         self.assertEqual(self.trans('yes, or no'), 'oui, ou non')
 
     def test_substitute(self):
-        self.assertEqual(self.trans('Welcome %(user)!', user='John'),
+        self.assertEqual(self.trans('Welcome %(user)s!', user='John'),
                          'Bienvenue John !')
