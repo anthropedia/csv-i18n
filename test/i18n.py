996ecb797e5e26f5bbd0597e37db2140189386c8
Vincent Agnano <vincent.agnano@scopyleft.fr>
2017-05-12 17:53:52 +0200

    preparing tests for multilanguages

--- test/i18n.py
+++ test/i18n.py
@@ -5,6 +5,7 @@ from tcii18n import trans
 class I18nTest(TestCase):
     def setUp(self):
         self.trans = trans._
+        self.trans_it = None
 
     def test_translate(self):
         self.assertEqual(self.trans('TCI title'), 'Titre ITC')
@@ -13,3 +14,8 @@ class I18nTest(TestCase):
     def test_substitute(self):
         self.assertEqual(self.trans('Welcome %(user)s!', user='John'),
                          'Bienvenue John !')
+
+    def test_switch_language(self):
+        self.assertEqual(self.trans_it('TCI title'), 'ITC titolo')
+        self.assertEqual(self.trans_it('Welcome %(user)s!', user='John'),
+                         'Benvenuto John!')
