56dcc94c2674085b37aa584cb5c22f8aad50ea7a
vinyll <vincent.agnano@scopyleft.fr>
2018-01-26 15:38:30 -0600

    removed template system and moved cache into Translator class

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 
 setup(
     name='tcii18n',
-    version='0.1',
-    author='Hugo Mochet',
+    version='0.2',
+    author='Vincent Agnano',
     license='Copyright Anthropedia',
     long_description=open('readme.md').read(),
 )
--- tcii18n/translator.py
+++ tcii18n/translator.py
@@ -1,16 +1,29 @@
 import csv
 
 
+_translators = {}
+
+
 class Translator:
-    def __init__(self, filename):
+    def __init__(self, filename, cache=True):
+        self._cache_key = filename
+        self._cache = cache
         reader = open(filename, 'r')
         self.content = tuple(csv.reader(reader))
         reader.close()
+        if self._cache and self._cache_key not in _translators.keys():
+            _translators[self._cache_key] = self.content
+
+    def get_translations(self):
+        if self._cache:
+            return _translators[self._cache_key]
+        else:
+            return self.content
 
     def translate(self, sentence, **kwargs):
-        for row in self.content:
-            if row[0] == sentence:
-                for key, value in kwargs.items():
-                    row[1] = row[1] % {key: value}
-                return row[1]
-        return sentence
+        translation = dict(self.get_translations()).get(sentence)
+        if not translation:
+            return sentence
+        for key, value in kwargs.items():
+            translation = translation % {key: value}
+        return translation
