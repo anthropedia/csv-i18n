847e64270bfd7f793d5c15f8bca6dafee350b5b4
Vincent Agnano <vinyll@protonmail.com>
2020-12-29 13:56:06 +0100

    allow fallback language

--- csvi18n/__init__.py
+++ csvi18n/__init__.py
@@ -5,14 +5,19 @@ _cached_translators = {}
 
 
 class Translator:
-    def __init__(self, filename: str, use_cache: bool=True):
-        self._cache_key = filename
-        self._translator = _chached_translators if use_cache else {}
-        reader = open(filename, 'r')
-        self.content = tuple(row for row in csv.reader(reader) if len(row))
+    def __init__(self, filename: str, use_cache: bool=True, fallback_path: str=None):
+        if fallback_path:
+            reader = open(fallback_path, 'r')
+            self.content.update(dict(row for row in csv.reader(reader) if len(row)))
+            reader.close()
+
+        reader = open(filepath, 'r')
+        self.content.update(dict(row for row in csv.reader(reader) if len(row)))
         reader.close()
+
         if self._cache and self._cache_key not in _translators.keys():
             _translators[self._cache_key] = self.content
+
 
     def get_translations(self) -> tuple:
         if self.use_cache:
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='csv-i18n',
-    version='0.2',
+    version='0.3',
     author='Vincent Agnano',
     license='Copyright Anthropedia',
     long_description=open('readme.md').read(),
